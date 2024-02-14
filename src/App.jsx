d05e1eadf9732e033bdbda7f43e9827b3b30f1ef
Anuska86 <anasrm86@gmail.com>
2024-02-14 15:17:02 +0100

    table

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,23 @@ const Statistics = (props) => {
   if (props.values.allClicks > 0) {
     return (
       <div>
-        <h1>Statistics</h1>
-        <StatisticLine title="All clicks" value={props.values.allClicks} />
-        <StatisticLine title="Average" value={props.values.average} />
-        <StatisticLine
-          title="Positive"
-          value={props.values.good / props.values.allClicks}
-        />
+        <table>
+          <tr>
+            <h1>Statistics</h1>
+          </tr>
+          <tr>
+            <StatisticLine title="All clicks" value={props.values.allClicks} />
+          </tr>
+          <tr>
+            <StatisticLine title="Average" value={props.values.average} />
+          </tr>
+          <tr>
+            <StatisticLine
+              title="Positive"
+              value={props.values.good / props.values.allClicks}
+            />
+          </tr>
+        </table>
       </div>
     );
   } else {
@@ -80,10 +90,23 @@ const App = () => {
   return (
     <div>
       <div>
-        <h1>Feedback</h1>
-        <Button name="Good" handle={handleGoodClick} value={good} />
-        <Button name="Neutral" handle={handleNeutralClick} value={neutral} />
-        <Button name="Bad" handle={handleBadClick} value={bad} />
+        <table>
+          <tr>
+            <th>
+              <h1>Feedback</h1>
+            </th>
+          </tr>
+
+          <tr>
+            <Button name="Good" handle={handleGoodClick} value={good} />
+          </tr>
+          <tr>
+            <Button name="Neutral"handle={handleNeutralClick}value={neutral}/>
+        </tr>
+          <tr>
+            <Button name="Bad" handle={handleBadClick} value={bad} />
+          </tr>
+        </table>
       </div>
       <Statistics values={values} />
     </div>
